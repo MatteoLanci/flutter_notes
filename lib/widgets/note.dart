fe302a9913fe8768dd43c70319b55bdaaba963b0
matteo lanci <m.lanci@live.com>
2024-02-28 16:45:06 +0100

    new dependecie: flutter_slidable to pubspec file + slidable integration in note.dart

--- lib/widgets/note.dart
+++ lib/widgets/note.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notes/update_page.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 
 class NoteWidget extends StatelessWidget {
@@ -22,24 +23,41 @@ class NoteWidget extends StatelessWidget {
     String formattedDate = DateFormat('dd/MM/yyyy HH:mm', 'it_IT').format(
         parsedDate); //? Formatta la data in un formato più leggibile, ad esempio "26/02/2024 18:25"
 
-    return ListTile(
-      title: Text(note['body']),
-      subtitle: Text('Updated: $formattedDate'),
-      trailing: IconButton(
-        onPressed: onDelete,
-        icon: const Icon(Icons.delete),
-      ),
-      onTap: () async {
-        final result = await Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (ctx) => UpdatePage(note: note),
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const DrawerMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) {
+              onDelete();
+            },
+            icon: Icons.delete,
+            backgroundColor: Colors.red,
           ),
-        );
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 50, top: 0, right: 0, bottom: 0),
+        child: ListTile(
+          title: Text(note['body']),
+          subtitle: Text('Updated: $formattedDate'),
+          // trailing: IconButton(
+          //   onPressed: onDelete,
+          //   icon: const Icon(Icons.delete),
+          // ),
+          onTap: () async {
+            final result = await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (ctx) => UpdatePage(note: note),
+              ),
+            );
 
-        if (result == true) {
-          getNotes();
-        }
-      },
+            if (result == true) {
+              getNotes();
+            }
+          },
+        ),
+      ),
     );
   }
 }
